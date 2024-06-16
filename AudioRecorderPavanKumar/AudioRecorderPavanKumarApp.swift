f3dc8091b3645ac2d88ae20a9b02250a820c3699
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-06-16 14:20:37 +0200

    Initial Commit

---
+++ AudioRecorderPavanKumar/AudioRecorderPavanKumarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioRecorderPavanKumarApp.swift
+//  AudioRecorderPavanKumar
+//
+//  Created by Pavankumar Arepu on 16/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AudioRecorderPavanKumarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
