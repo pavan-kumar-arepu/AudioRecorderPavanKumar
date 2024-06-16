60e364ae28be468fb9cac0915fa77a483dc62f60
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-06-16 23:58:58 +0200

    Documentation completed for RecordingListViewModel

--- AudioRecorderPavanKumar/ViewModels/RecordingListViewModel.swift
+++ AudioRecorderPavanKumar/ViewModels/RecordingListViewModel.swift
@@ -7,13 +7,23 @@
 
 import Foundation
 
+
+/// ViewModel for managing a list of recorded audio files.
+///
+/// This class provides functionality to fetch, rename, and delete recorded audio files
+/// stored in the document directory.
+///
+/// - Author: Arepu Pavan Kumar
 class RecordingsListViewModel: ObservableObject {
+    /// Published property to observe changes in the list of recorded audio file URLs.
     @Published var recordings: [URL] = []
     
+    /// Initializes the `RecordingsListViewModel` and fetches recorded audio files.
     init() {
         fetchRecordings()
     }
     
+    /// Fetches the list of recorded audio files from the document directory.
     func fetchRecordings() {
         let documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
         do {
@@ -24,6 +34,10 @@ class RecordingsListViewModel: ObservableObject {
         }
     }
     
+    /// Renames the recorded audio file at a specified index.
+    /// - Parameters:
+    ///   - index: Index of the recording in the `recordings` array.
+    ///   - newName: New name to assign to the recording.
     func renameRecording(at index: Int, to newName: String) {
         let oldURL = recordings[index]
         let newURL = oldURL.deletingLastPathComponent().appendingPathComponent("\(newName).m4a")
@@ -37,6 +51,8 @@ class RecordingsListViewModel: ObservableObject {
         }
     }
     
+    /// Deletes the recorded audio file at a specified index.
+    /// - Parameter index: Index of the recording in the `recordings` array.
     func deleteRecording(at index: Int) {
         let recordingURL = recordings[index]
         
